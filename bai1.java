c16c3c97c4bd0ad355988ed07262d454fd1e0764
Tran Duc Duy <148021115+dy1307@users.noreply.github.com>
2026-08-20 08:31:37 +0700

    Add files via upload

---
+++ bai1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Hello World");
+        scanner.close();
+    }
+}
